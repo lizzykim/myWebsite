b594c71877b64c70f98fb25c94d4fa1098ab3fff
lizzykim <kjdayoung@gmail.com>
2022-12-13 23:43:48 +0900

    chore: implement GlobalStyle with styled-component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MyGlobalStyle } from '.';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <MyGlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
