f8dc1a830fd597bccfcd5d86623c1493ebc71b0a
lizzykim <kjdayoung@gmail.com>
2022-12-27 22:17:34 +0900

    feat: add footer

---
+++ src/components/Footer/footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledText = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70px;
+  color: ${(props) => props.theme.color.paragraph};
+`;
+
+const Footer = () => {
+  return <StyledText>Designed and Built by Dayoung Kim 🚀</StyledText>;
+};
+
+export default Footer;
