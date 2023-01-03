2f91eb88720622e3ba46d6d76bf9adcf8cf41bc3
lizzykim <kjdayoung@gmail.com>
2023-01-03 22:49:21 +0900

    chore: add colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,7 +1,9 @@
 const colors = {
   beige: '#FEF6E4',
+  dark_beige: '#d4cbba',
   navy: '#001858',
   light_navy: '#001858',
+  dark_navy: '#141624',
   light_navy_2: '#B8C1EC',
   pink: '#F582AE',
   baby_pink: '#EEBBC3',
