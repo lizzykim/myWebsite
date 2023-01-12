ab627bb37d828dbcf6d931796a53deaa331dd235
lizzykim <kjdayoung@gmail.com>
2023-01-12 22:24:29 +0900

    feat: add About component & add ScrollReveal with next-reveal

--- src/config.ts
+++ src/config.ts
@@ -9,4 +9,20 @@ const navLinks: navLinkProps[] = [
   { name: 'Contact' },
 ];
 
-export { navLinks };
+const scrollRevealOptions = {
+  origin: 'bottom',
+  distance: '50px',
+  duration: 800,
+  delay: 200,
+  rotate: { x: 0, y: 0, z: 0 },
+  opacity: 0,
+  scale: 1,
+  easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+  mobile: true,
+  reset: false,
+  useDelay: 'always',
+  viewFactor: 0.25,
+  viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+};
+
+export { navLinks, scrollRevealOptions };
